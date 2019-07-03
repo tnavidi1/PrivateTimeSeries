48f91a39f9b62577cb1efaa6903376bf565916de
Xiao (Mark) Chen <mark.chen.jq@gmail.com>
2019-07-02 23:10:06 -0700

    add data process

--- CaseStudy_IrishCER/processData.py
+++ CaseStudy_IrishCER/processData.py
@@ -8,9 +8,6 @@ import metersUtil
 
 # "ISO-8859-1"
 
-reformatDF = pd.read_csv("../../Irish_CER_data_formated/reformated_File1.txt",  index_col=None,
-                         encoding="iso-8859-1", sep=',',
-                         low_memory=False, error_bad_lines=False, nrows=None) #24465540
 
 ############
 # separate #
@@ -43,35 +40,65 @@ reformatDF = pd.read_csv("../../Irish_CER_data_formated/reformated_File1.txt",  
 
 
 
-df_income_id_label = metersUtil.load_static_attr('income')
-
-i=0
-batch_size = 150
-for row in df_income_id_label.itertuples():
-    i += 1
-    # print(getattr(row, "ID"), getattr(row, "income_level"))
-    mid = str(int(getattr(row, "ID")))
-    income = int(getattr(row, "income_level"))
-    mid_ts = metersUtil.iterateMeter(mid, reformatDF)
-    # ==========================
-
-    mid_ts_df = pd.DataFrame({'Date': mid_ts.index.date, 'Hr': mid_ts.index.hour,
-                              'Min': mid_ts.index.minute,
-                              'Step': (np.round(mid_ts.index.hour.astype(float) + mid_ts.index.minute.astype(float)/60.0, 1) * 2).astype(int),
-                              'Elec_KW': mid_ts.values})
-    # reshape to one-day format
-    daily_KW_matrix = mid_ts_df.pivot(index='Date', columns='Step', values='Elec_KW').dropna()
-    nrows = daily_KW_matrix.shape[0]
-    np.random.seed(1)
-    nsamples = np.random.choice(nrows, size=min(batch_size, nrows-1), replace=False)
-    df_mid_ts_attr_sub = pd.DataFrame({'Date': daily_KW_matrix.index[nsamples],
-                                   'Income': np.repeat(income, min(batch_size, nrows-1))})
-
-    daily_KW_matrix_sub=(daily_KW_matrix.iloc[nsamples, :].reset_index(level=['Date']))
-
-    mid_batch_samples = pd.merge(daily_KW_matrix_sub, df_mid_ts_attr_sub, on='Date', how='inner')
-
-    if i > 2:
-        break
 
 
+
+
+def parse_train_test(dir_root="../../Irish_CER_data_formated",
+                     filename="reformated_File1.txt", nsample_per_mid=150):
+    """
+
+    :param dir_root:
+    :param filename:
+    :param nsample_per_mid:
+    :return:
+    """
+    reformatDF = pd.read_csv(os.path.join(dir_root, filename), index_col=None,
+                             encoding="iso-8859-1", sep=',',
+                             low_memory=False, error_bad_lines=False, nrows=None)  # 24465540
+
+    df_income_id_label = metersUtil.load_static_attr('income')
+
+    i = 0
+    batch_size = nsample_per_mid
+    X = None
+    Y = None
+    for row in df_income_id_label.itertuples():
+        i += 1
+        # print(getattr(row, "ID"), getattr(row, "income_level"))
+        mid = str(int(getattr(row, "ID")))
+        income = int(getattr(row, "income_level"))
+        mid_ts = metersUtil.iterateMeter(mid, reformatDF)
+        # ==========================
+
+        mid_ts_df = pd.DataFrame({'Date': mid_ts.index.date, 'Hr': mid_ts.index.hour,
+                                  'Min': mid_ts.index.minute,
+                                  'Step': (np.round(mid_ts.index.hour.astype(float) + mid_ts.index.minute.astype(float)/60.0, 1) * 2).astype(int),
+                                  'Elec_KW': mid_ts.values})
+        # reshape to one-day format
+        daily_KW_matrix = mid_ts_df.pivot(index='Date', columns='Step', values='Elec_KW').dropna()
+        nrows = daily_KW_matrix.shape[0]
+        np.random.seed(1)
+        nsamples = np.random.choice(nrows, size=min(batch_size, nrows-1), replace=False)
+        df_mid_ts_attr_sub = pd.DataFrame({'Date': daily_KW_matrix.index[nsamples],
+                                       'Income': np.repeat(income, min(batch_size, nrows-1))})
+
+        daily_KW_matrix_sub=(daily_KW_matrix.iloc[nsamples, :].reset_index(level=['Date']))
+
+        mid_batch_samples = pd.merge(daily_KW_matrix_sub, df_mid_ts_attr_sub, on='Date', how='inner')
+        # cols: date , 1, 2..., 48(power), 49(label)
+        if X is None and Y is None:
+            X = mid_batch_samples.iloc[:, 1:49].to_numpy()
+            Y = mid_batch_samples.iloc[:, 49:50].to_numpy()
+        else:
+            X = np.vstack((X, mid_batch_samples.iloc[:, 1:49].to_numpy() ))
+            Y = np.vstack((Y, mid_batch_samples.iloc[:, 49:50].to_numpy() ) )
+
+
+        # if i > 2:
+        #     print(X)
+        #     print(Y)
+        #     break
+
+
+parse_train_test()
