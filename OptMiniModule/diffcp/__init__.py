efa1e87f5f05181776de081a62e0f88efa303885
Xiao (Mark) Chen <mark.chen.jq@gmail.com>
2019-08-23 00:35:31 -0700

    nesty import TO BE Fixed

---
+++ OptMiniModule/diffcp/__init__.py
@@ -0,0 +1,8 @@
+# __all__ = ['cone_program', 'cones']
+
+# from . import cones
+# from . import cone_program
+
+# from .cone_program import solve_and_derivative, solve_and_derivative_batch
+# from .cone_program import *
+# from .cones import ZERO, POS, SOC, PSD, EXP
